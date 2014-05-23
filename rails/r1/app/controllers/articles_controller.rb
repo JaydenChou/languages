92c1f654dd5b06195839e6c423e1c289a71610c8
Javier Fuchs <javier.fuchs@gmail.com>
2014-05-22 22:40:03 -0300

    articles/index

--- rails/r1/app/controllers/articles_controller.rb
+++ rails/r1/app/controllers/articles_controller.rb
@@ -11,9 +11,13 @@ class ArticlesController < ApplicationController
 		redirect_to @article
 	end
 
+	def index
+		@articles = Article.all
+	end
+
 	def show
 		@article = Article.find(params[:id])
-	@end
+	end
 
 	private
 		def article_params
