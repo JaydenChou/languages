3de0fbfbaa201dfed8b1f47f4f9a1131a6a2f239
Javier Fuchs <javier.fuchs@gmail.com>
2014-05-22 22:58:54 -0300

    Articles edit, new, show

--- rails/r1/app/controllers/articles_controller.rb
+++ rails/r1/app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 	def new
+		@article = Article.new
 	end
 
 	def create
@@ -7,8 +8,11 @@ class ArticlesController < ApplicationController
 
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
 
 	def index
@@ -18,6 +22,21 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+			
 
 	private
 		def article_params
